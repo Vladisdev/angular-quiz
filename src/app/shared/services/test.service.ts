c885fdc273726765623d533c6be1276c3e62d98b
Владислав Попов <vladislavpopov.dev@gmail.com>
2023-03-03 21:28:03 +0300

    add tests behaviour with results

--- src/app/shared/services/test.service.ts
+++ src/app/shared/services/test.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { QuizListType } from '../../../types/quiz-list.type';
 import { Observable } from 'rxjs';
+import { TestResultType } from '../../../types/test-result.type';
+import { DefaultResponseType } from '../../../types/default-response.type';
 
 @Injectable({
   providedIn: 'root',
@@ -12,5 +14,12 @@ export class TestService {
 
   getTests(): Observable<QuizListType[]> {
     return this.http.get<QuizListType[]>(environment.apiHost + 'tests');
+  }
+  getUserResults(
+    userId: number
+  ): Observable<DefaultResponseType | TestResultType[]> {
+    return this.http.get<DefaultResponseType | TestResultType[]>(
+      environment.apiHost + 'tests/results?userId=' + userId
+    );
   }
 }
--- src/app/views/test/choice/choice.component.ts
+++ src/app/views/test/choice/choice.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { TestService } from '../../../shared/services/test.service';
 import { QuizListType } from '../../../../types/quiz-list.type';
+import { AuthService } from '../../../core/auth/auth.service';
+import { DefaultResponseType } from '../../../../types/default-response.type';
+import { TestResultType } from '../../../../types/test-result.type';
 
 @Component({
   selector: 'app-choice',
@@ -9,12 +12,41 @@ import { QuizListType } from '../../../../types/quiz-list.type';
 })
 export class ChoiceComponent implements OnInit {
   quizzes: QuizListType[] = [];
+  private testResult: TestResultType[] | null = null;
 
-  constructor(private testService: TestService) {}
+  constructor(
+    private testService: TestService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.testService.getTests().subscribe((response: QuizListType[]) => {
       this.quizzes = response;
     });
+
+    const userInfo = this.authService.getUserInfo();
+    if (userInfo) {
+      this.testService.getUserResults(userInfo.userId).subscribe((data) => {
+        if (data) {
+          if ((data as DefaultResponseType).error !== undefined) {
+            throw new Error((data as DefaultResponseType).message);
+          }
+
+          const testResult = data as TestResultType[];
+          if (testResult) {
+            this.quizzes = this.quizzes.map((quiz: QuizListType) => {
+              const foundItem: TestResultType | undefined = testResult?.find(
+                (item: TestResultType) => item.testId === quiz.id
+              );
+              if (foundItem) {
+                quiz.result = foundItem.score + '/' + foundItem.total;
+              }
+
+              return quiz;
+            });
+          }
+        }
+      });
+    }
   }
 }
