6b9bde54cca72da9630dd5ba76c2f570894de412
Владислав Попов <vladislavpopov.dev@gmail.com>
2023-03-03 21:37:25 +0300

    hotfix

--- src/app/views/test/choice/choice.component.ts
+++ src/app/views/test/choice/choice.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { TestService } from '../../../shared/services/test.service';
 import { QuizListType } from '../../../../types/quiz-list.type';
 import { AuthService } from '../../../core/auth/auth.service';
-import { DefaultResponseType } from '../../../../types/default-response.type';
 import { TestResultType } from '../../../../types/test-result.type';
+import { DefaultResponseType } from '../../../../types/default-response.type';
 
 @Component({
   selector: 'app-choice',
@@ -22,31 +22,31 @@ export class ChoiceComponent implements OnInit {
   ngOnInit(): void {
     this.testService.getTests().subscribe((response: QuizListType[]) => {
       this.quizzes = response;
+
+      const userInfo = this.authService.getUserInfo();
+      if (userInfo) {
+        this.testService.getUserResults(userInfo.userId).subscribe((data) => {
+          if (data) {
+            if ((data as DefaultResponseType).error !== undefined) {
+              throw new Error((data as DefaultResponseType).message);
+            }
+
+            const testResult = data as TestResultType[];
+            if (testResult) {
+              this.quizzes = this.quizzes.map((quiz: QuizListType) => {
+                const foundItem: TestResultType | undefined = testResult?.find(
+                  (item: TestResultType) => item.testId === quiz.id
+                );
+                if (foundItem) {
+                  quiz.result = foundItem.score + '/' + foundItem.total;
+                }
+
+                return quiz;
+              });
+            }
+          }
+        });
+      }
     });
-
-    const userInfo = this.authService.getUserInfo();
-    if (userInfo) {
-      this.testService.getUserResults(userInfo.userId).subscribe((data) => {
-        if (data) {
-          if ((data as DefaultResponseType).error !== undefined) {
-            throw new Error((data as DefaultResponseType).message);
-          }
-
-          const testResult = data as TestResultType[];
-          if (testResult) {
-            this.quizzes = this.quizzes.map((quiz: QuizListType) => {
-              const foundItem: TestResultType | undefined = testResult?.find(
-                (item: TestResultType) => item.testId === quiz.id
-              );
-              if (foundItem) {
-                quiz.result = foundItem.score + '/' + foundItem.total;
-              }
-
-              return quiz;
-            });
-          }
-        }
-      });
-    }
   }
 }
